db4d88c65fda68253e3f6a57c1280f6b99c8ce38
nerviozzo <raupulus@gmail.com>
2015-05-22 20:45:49 +0200

    Limpieza de código.

--- JS/selectorimagenespintarbackground.js
+++ JS/selectorimagenespintarbackground.js
@@ -1,52 +1,57 @@
 //Función para dibujar en el background "contenido" la imagen seleccionada según el nombre de la imagen
-function muestra(nombre)
-         {
+function muestra(nombre) {
 				//Asigno variable nombre para la ruta del archivo
-            var x = 'url' + "(" + "'" + nombre + "'" + ")";
-
-				var video = document.getElementById("videopresentacion")
-
-				//Establezco las propiedades CSS
+	var x = "url" + "(" + "'" + nombre + "'" + ")";
+	var video = document.getElementById("videopresentacion");
+	var contenido = document.getElementById("contenido");
 
 					//Ocultar video y detenerlo al pulsar imagen
-						document.getElementById("videopresentacion").style.opacity = 0
-						document.getElementById("videopresentacion").style.visibility = 0
-						video.zIndex = -10;
-						video.pause()
+	video.style.opacity = 0;
+	video.style.visibility = 0;
+	video.zIndex = -10;
+	video.pause();
 
+		//Establezco las propiedades CSS
 					//Dibujar background
-						document.getElementById("contenido").style.background = x
-						document.getElementById("contenido").style.backgroundSize = "contain"
-						document.getElementById("contenido").style.backgroundRepeat = "no-repeat"
-						document.getElementById("contenido").style.backgroundColor = "black"
-						document.getElementById("contenido").style.backgroundPosition = "50% 50%"
-            }
+	contenido.style.background = x;
+	contenido.style.backgroundSize = "contain";
+	contenido.style.backgroundRepeat = "no-repeat";
+	contenido.style.backgroundColor = "black";
+	contenido.style.backgroundPosition = "50% 50%";
+}
 
 //Funciones para los controles de imágenes
 
 function proximaimg() {
-		var imgactual = document.getElementById("contenido").style.backgroundImage
-		alert(imgactual)
-	}
+	var imgactual = document.getElementById("contenido").style.backgroundImage;
+	window.alert(imgactual);
+}
 
 function anteriorimg() {
-		var imgactual = document.getElementById("contenido").style.backgroundImage
-		alert(imgactual)
-	}
+	var imgactual = document.getElementById("contenido").style.backgroundImage;
+	window.alert(imgactual);
+}
 
-//Función para parar y silenciar vídeo
+//Función para pausar el video de presentación
 function videoplay() {
-       var video = document.getElementById("videopresentacion");
-       var button = document.getElementById("play");
-       if (video.paused) {
-          video.style.opacity = 1;
-			 video.zIndex = 0;
-			 document.getElementById("contenido").style.background = "url('')" ;
-			 document.getElementById("contenido").style.backgroundColor = "black" ;
-			 video.play();
-          button.textContent = "||";
-       } else {
-          video.pause();
-          button.textContent = ">";
-       }
-    }
+	var video = document.getElementById("videopresentacion");
+	var button = document.getElementById("play");
+	var botonplay = document.getElementById("botonplay");
+
+	if (video.paused) {
+		video.style.opacity = 1;
+		video.zIndex = 0;
+		document.getElementById("contenido").style.background = "url('')" ;
+		document.getElementById("contenido").style.backgroundColor = "black" ;
+		video.play();
+      //button.textContent = "||";
+		botonplay.src = "./images/botonstop.png";
+	} else {
+		video.pause();
+		//button.textContent = ">";
+		botonplay.src = "./images/botonplay.png";
+	}
+}
+
+//Función solo para silenciar el audio
+
