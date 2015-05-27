531de1a196d0a539b29d31ecc2f0b00bc71a3fc6
nerviozzo <raupulus@gmail.com>
2015-05-28 00:16:26 +0200

    Creando función que al desplegar cada menu superior escriba una lista con los enlaces correspondientes que sea mostrada

--- JS/scripts.js
+++ JS/scripts.js
@@ -167,32 +167,98 @@ function noampliar() {
 
 //Función para mostrar Descripción junto a las imágenes del menu desplegable a la izquierda siendo "n" el numero del desplegable
 function descripcion(n) {
-	x =  n + "desplegableletras"
+	x =  n + "desplegableletras";
 
 				//Establezco las propiedades CSS
-	document.getElementById(x).style.opacity = "1"
+	document.getElementById(x).style.opacity = "1";
 }
 
 //Función para "mouseout" que cierra la descripción cuando pierde el foco
 function nodescripcion() {
-	document.getElementById(x).style.opacity = "0"
+	document.getElementById(x).style.opacity = "0";
 }
 
 //----------------------------------------------------MENU DESPLEGABLE MENU SUPERIOR FIXED-------------------------
-function menusuperiordesplegable() {
-	var desplegar = document.getElementById('caja1')
-	var plegar = desplegar.onmouseout
+//Desplegable para menu superior que sea accionado con el over del ratón y termine al perder el foco volviendo a dejar display=none
+function menusuperiordesplegable(id) {
+	var nombredemenu = document.getElementById(id);
 
-	desplegar.style.display = 	"block"
-	plegar.style.display = "none"
+	nombredemenu.style.display = 	"block";
+
+// Escribir enlaces dentro del menu superior "album"
+	var lista = '<UL id="uldelista2" class="ulcomunlistas">';
+	lista += '<LI class="lidesplegable"><A href="flora_general.html" class="linkmenusuperior">Flora</A></LI>';
+	lista += '<LI class="lidesplegable"><A href="fauna_general.html" class="linkmenusuperior">Fauna</A></LI>'
+	lista += '<LI class="lidesplegable"><A href="playas_general.html" class="linkmenusuperior">Playas</A></LI>'
+	lista += '<LI class="lidesplegable"><A href="monumentos_general.html" class="linkmenusuperior">Monumentos</A></LI>'
+	lista += '<LI class="lidesplegable"><A href="puestas_de_sol.html" class="linkmenusuperior">Puestas de sol</A></LI>'
+	lista += '<LI class="lidesplegable"><A href="fiestas_general.html" class="linkmenusuperior">Fiestas y Eventos</A></LI>'
+	lista += '</UL>';
+	document.getElementById('albumcaja3').innerHTML = lista;
+
+	/* Este es el codigo HTML que ha de importarse mediante javascript al hacer hover sobre su menú correspondiente
+
+
+			<UL id="uldesplegable1" class="uldesplegable"><!--Desplegable1-->
+				<LI class="titulodesplegable"><A class="linktitulo" href="./index.html">INICIO</A></LI>
+			</UL>
+
+			<UL id="uldesplegable2" class="uldesplegable"><!--Desplegable 2-->
+				<LI class="titulodesplegable"><A class="linktitulo" href="./index.html">ALBUMS</A>
+					<UL id="uldelista2" class="ulcomunlistas">
+						<LI class="lidesplegable"><A href="flora_general.html" class="linkmenusuperior">Flora</A></LI>
+						<LI class="lidesplegable"><A href="fauna_general.html" class="linkmenusuperior">Fauna</A></LI>
+                  <LI class="lidesplegable"><A href="playas_general.html" class="linkmenusuperior">Playas</A></LI>
+						<LI class="lidesplegable"><A href="monumentos_general.html" class="linkmenusuperior">Monumentos</A></LI>
+						<LI class="lidesplegable"><A href="puestas_de_sol.html" class="linkmenusuperior">Puestas de sol</A></LI>
+						<LI class="lidesplegable"><A href="fiestas_general.html" class="linkmenusuperior">Fiestas y Eventos</A></LI>
+					</UL>
+				</LI>
+			</UL>
+
+			<UL id="uldesplegable3" class="uldesplegable"><!--Desplegable 3-->
+				<LI class="titulodesplegable"><A class="linktitulo" href="./index.html">VÍDEOS</A>
+					<UL id="uldelista3" class="ulcomunlistas">
+						<LI class="lidesplegable"><A href="https://www.youtube.com/channel/UCIoK1tt5RChTC_-zEcEfDoQ" class="linkmenusuperior">Youtube Canal</A></LI>
+					</UL>
+				</LI>
+			</UL>
+
+			<UL id="uldesplegable4" class="uldesplegable"><!--Desplegable 4-->
+				<LI class="titulodesplegable"><A class="linktitulo" href="./index.html">WEBS AMIGAS</A>
+					<UL id="uldelista4" class="ulcomunlistas">
+                       <LI class="lidesplegable"><A href="http://www.laguialinux.es" class="linkmenusuperior">LaGuíaLinux</A></LI>
+                       <LI class="lidesplegable"><A href="http://www.exateunasrisas.nzweb.org" class="linkmenusuperior">Exate unas risas</A></LI>
+                       <LI class="lidesplegable"><A href="http://www.winkle.nzweb.org" class="linkmenusuperior">Asociación Winkle</A></LI>
+                       <LI class="lidesplegable"><A href="http://www.nzweb.org" class="linkmenusuperior">NzWeb, mi gran proyecto</A></LI>
+					</UL>
+				</LI>
+			</UL>
+
+			<UL id="uldesplegable5" class="uldesplegable"><!--Desplegable 5-->
+				<LI class="titulodesplegable"><A class="linktitulo" href="http://www.blog.desdedechipiona.nzweb.org">ÚLTIMAS FOTOS</A></LI>
+			</UL>
+
+			<UL id="uldesplegable6" class="uldesplegable"><!--Desplegable 6-->
+				<LI class="titulodesplegable"><A class="linktitulo" href="./contacto.html">+INFO</A>
+					<UL id="uldelista6" class="ulcomunlistas">
+						<LI class="lidesplegable"><A href="./contacto.html" class="linkmenusuperior">Contacto</A></LI>
+						<LI class="lidesplegable"><A href="./about.html" class="linkmenusuperior">Sobre este proyecto</A></LI>
+					</UL>
+				</LI>
+			</UL>
+
+	*/
 }
 
-function menusuperiornodesplegable() {
-	var desplegar = document.getElementById('caja1')
+function menusuperiornodesplegable(id) {
+	var nombredemenu = document.getElementById(id);
 
-
-	desplegar.style.display = "none"
-
+	nombredemenu.style.display = "none";
 }
+
+
+
+
 
 
