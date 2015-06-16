459ffa1871292fc4b06b5d6303f5c866984ba5f0
nerviozzo1 <nerviozzo06@gmail.com>
2015-06-16 15:39:19 +0200

    Creada función para cambiar las imágenes de la galería; Empezada función para pintar background en id-contenido con calidad media

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,7 +3,7 @@
 //Función para dibujar en el background "contenido" la imagen seleccionada según el nombre de la imagen
 function muestrageneral(nombre) {
 				//Asigno variable nombre para la ruta del archivo
-	var rutadeimagen = "url" + "(" + "'" + "./images/1_chipiona_general/medium/" + nombre + ".jpg'" + ")";
+   var rutadeimagen = "url" + "(" + "'" + "./images/1_chipiona_general/medium/" + nombre + ".jpg'" + ")";
 	var video = document.getElementById("videopresentacion");
 	var contenido = document.getElementById("contenido");
 
@@ -18,11 +18,30 @@ function muestrageneral(nombre) {
 					//Guardar nombre de la imagen en "id=nombredeimagenactual"
 	document.getElementById("nombredeimagenactual").innerHTML = nombre;
 					//Dibujar background
-	contenido.style.background = rutadeimagen;
-	contenido.style.backgroundSize = "contain";
-	contenido.style.backgroundRepeat = "no-repeat";
-	contenido.style.backgroundColor = "black";
-	contenido.style.backgroundPosition = "50% 50%";
+
+
+
+//Creando funcion para que al pulsar sobre thumbnail lo dibuje en grande
+   var x;
+   var primeraimagendelselector = document.getElementById('selectorimagen1').style.backgroundImage;
+   var selectorprincipal = "url" + "(" + "'" + "./images/1_chipiona_general/thumbnail/" + 1 + ".jpg'" + ")";
+
+   //alert(primeraimagendelselector)
+
+   if (primeraimagendelselector == selectorprincipal || primeraimagendelselector == ''){
+      x = "url" + "(" + "'" + "./images/1_chipiona_general/medium/" + nombre + ".jpg'" + ")";
+
+      alert(selectorprincipal);
+
+      contenido.style.background = x;
+	  contenido.style.backgroundSize = "contain";
+	  contenido.style.backgroundRepeat = "no-repeat";
+	  contenido.style.backgroundColor = "black";
+	  contenido.style.backgroundPosition = "50% 50%";
+   }
+
+
+
 }
 
 //------------------------------------------Funciones para los controles de imágenes-------------------
@@ -233,4 +252,339 @@ function iniciarmenu() {
 }
 
 
+//----------------------------------------------Cambiar Galería de Imágenes-----------------------------------------
+//Función para seleccionar todos las clases del selector de imágenes
+function claseseleccion() {
+   var claseselector = document.getElementsByClassName('selectorimagen');
+      var x;
 
+      for (x = 0; x < claseselector.length; x++) {
+         claseselector[x].style.backgroundRepeat = "no-repeat";
+         claseselector[x].style.backgroundSize = "auto";
+         claseselector[x].style.backgroundPosition = "center";
+      }
+}
+
+//Función para cargar las nuevas imágenes en el selector de imágenes
+function rotargaleria(imagen) {
+//Común
+   var imagen;
+   var fin = ".jpg'" + ")";
+
+   var img1 = document.getElementById('selectorimagen1');
+   var img2 = document.getElementById('selectorimagen2');
+   var img3 = document.getElementById('selectorimagen3');
+   var img4 = document.getElementById('selectorimagen4');
+   var img5 = document.getElementById('selectorimagen5');
+   var img6 = document.getElementById('selectorimagen6');
+   var img7 = document.getElementById('selectorimagen7');
+   var img8 = document.getElementById('selectorimagen8');
+   var img9 = document.getElementById('selectorimagen9');
+   var img10 = document.getElementById('selectorimagen10');
+   var img11 = document.getElementById('selectorimagen11');
+   var img12 = document.getElementById('selectorimagen12');
+   var img13 = document.getElementById('selectorimagen13');
+   var img14 = document.getElementById('selectorimagen14');
+   var img15 = document.getElementById('selectorimagen15');
+   var img16 = document.getElementById('selectorimagen16');
+   var img17 = document.getElementById('selectorimagen17');
+   var img18 = document.getElementById('selectorimagen18');
+   var img19 = document.getElementById('selectorimagen19');
+   var img20 = document.getElementById('selectorimagen20');
+
+//Variables individuales
+   var principal = "url" + "(" + "'" + "./images/1_chipiona_general/thumbnail/";
+   var playageneral = "url" + "(" + "'" + "./images/2_playas/1_general/thumbnail/";
+   var playaderegla = "url" + "(" + "'" + "./images/2_playas/2_playa_de_regla/thumbnail/";
+   var playacruzdelmar = "url" + "(" + "'" + "./images/2_playas/3_playa_cruz_del_mar/thumbnail/";
+   var playadelmolino = "url" + "(" + "'" + "./images/2_playas/4_playa_del_molino/thumbnail/";
+   var playalascanteras = "url" + "(" + "'" + "./images/2_playas/5_playa_las_canteras/thumbnail/";
+   var monumentos = "url" + "(" + "'" + "./images/3_monumentos/thumbnail/";
+   var flora = "url" + "(" + "'" + "./images/4_flora/thumbnail/";
+   var faunageneral = "url" + "(" + "'" + "./images/5_fauna/1_general/thumbnail/";
+   var faunacamaleones = "url" + "(" + "'" + "./images/5_fauna/2_camaleones/thumbnail/";
+   var fiestasyeventos = "url" + "(" + "'" + "./images/6_fiestas_y_eventos/1_general/thumbnail/";
+   var puestasdesol = "url" + "(" + "'" + "./images/7_puestas_de_sol/thumbnail/";
+
+   if (imagen == "principal") {
+//Página principal
+      img1.style.background = principal + "1" + fin;
+      img2.style.background = principal + "2" + fin;
+      img3.style.background = principal + "3" + fin;
+      img4.style.background = principal + "4" + fin;
+      img5.style.background = principal + "5" + fin;
+      img6.style.background = principal + "6" + fin;
+      img7.style.background = principal + "7" + fin;
+      img8.style.background = principal + "8" + fin;
+      img9.style.background = principal + "9" + fin;
+      img10.style.background = principal + "10" + fin;
+      img11.style.background = principal + "11" + fin;
+      img12.style.background = principal + "12" + fin;
+      img13.style.background = principal + "13" + fin;
+      img14.style.background = principal + "14" + fin;
+      img15.style.background = principal + "15" + fin;
+      img16.style.background = principal + "16" + fin;
+      img17.style.background = principal + "17" + fin;
+      img18.style.background = principal + "18" + fin;
+      img19.style.background = principal + "19" + fin;
+      img20.style.background = principal + "20" + fin;
+      claseseleccion()
+
+   } else if (imagen == "playageneral"){
+//Playa_general
+      img1.style.background = playageneral + "1" + fin;
+      img2.style.background = playageneral + "2" + fin;
+      img3.style.background = playageneral + "3" + fin;
+      img4.style.background = playageneral + "4" + fin;
+      img5.style.background = playageneral + "5" + fin;
+      img6.style.background = playageneral + "6" + fin;
+      img7.style.background = playageneral + "7" + fin;
+      img8.style.background = playageneral + "8" + fin;
+      img9.style.background = playageneral + "9" + fin;
+      img10.style.background = playageneral + "10" + fin;
+      img11.style.background = playageneral + "11" + fin;
+      img12.style.background = playageneral + "12" + fin;
+      img13.style.background = playageneral + "13" + fin;
+      img14.style.background = playageneral + "14" + fin;
+      img15.style.background = playageneral + "15" + fin;
+      img16.style.background = playageneral + "16" + fin;
+      img17.style.background = playageneral + "17" + fin;
+      img18.style.background = playageneral + "18" + fin;
+      img19.style.background = playageneral + "19" + fin;
+      img20.style.background = playageneral + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "playaderegla") {
+      img1.style.background = playaderegla + "1" + fin;
+      img2.style.background = playaderegla + "2" + fin;
+      img3.style.background = playaderegla + "3" + fin;
+      img4.style.background = playaderegla + "4" + fin;
+      img5.style.background = playaderegla + "5" + fin;
+      img6.style.background = playaderegla + "6" + fin;
+      img7.style.background = playaderegla + "7" + fin;
+      img8.style.background = playaderegla + "8" + fin;
+      img9.style.background = playaderegla + "9" + fin;
+      img10.style.background = playaderegla + "10" + fin;
+      img11.style.background = playaderegla + "11" + fin;
+      img12.style.background = playaderegla + "12" + fin;
+      img13.style.background = playaderegla + "13" + fin;
+      img14.style.background = playaderegla + "14" + fin;
+      img15.style.background = playaderegla + "15" + fin;
+      img16.style.background = playaderegla + "16" + fin;
+      img17.style.background = playaderegla + "17" + fin;
+      img18.style.background = playaderegla + "18" + fin;
+      img19.style.background = playaderegla + "19" + fin;
+      img20.style.background = playaderegla + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "playacruzdelmar") {
+      img1.style.background = playacruzdelmar + "1" + fin;
+      img2.style.background = playacruzdelmar + "2" + fin;
+      img3.style.background = playacruzdelmar + "3" + fin;
+      img4.style.background = playacruzdelmar + "4" + fin;
+      img5.style.background = playacruzdelmar + "5" + fin;
+      img6.style.background = playacruzdelmar + "6" + fin;
+      img7.style.background = playacruzdelmar + "7" + fin;
+      img8.style.background = playacruzdelmar + "8" + fin;
+      img9.style.background = playacruzdelmar + "9" + fin;
+      img10.style.background = playacruzdelmar + "10" + fin;
+      img11.style.background = playacruzdelmar + "11" + fin;
+      img12.style.background = playacruzdelmar + "12" + fin;
+      img13.style.background = playacruzdelmar + "13" + fin;
+      img14.style.background = playacruzdelmar + "14" + fin;
+      img15.style.background = playacruzdelmar + "15" + fin;
+      img16.style.background = playacruzdelmar + "16" + fin;
+      img17.style.background = playacruzdelmar + "17" + fin;
+      img18.style.background = playacruzdelmar + "18" + fin;
+      img19.style.background = playacruzdelmar + "19" + fin;
+      img20.style.background = playacruzdelmar + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "playadelmolino") {
+      img1.style.background = playadelmolino + "1" + fin;
+      img2.style.background = playadelmolino + "2" + fin;
+      img3.style.background = playadelmolino + "3" + fin;
+      img4.style.background = playadelmolino + "4" + fin;
+      img5.style.background = playadelmolino + "5" + fin;
+      img6.style.background = playadelmolino + "6" + fin;
+      img7.style.background = playadelmolino + "7" + fin;
+      img8.style.background = playadelmolino + "8" + fin;
+      img9.style.background = playadelmolino + "9" + fin;
+      img10.style.background = playadelmolino + "10" + fin;
+      img11.style.background = playadelmolino + "11" + fin;
+      img12.style.background = playadelmolino + "12" + fin;
+      img13.style.background = playadelmolino + "13" + fin;
+      img14.style.background = playadelmolino + "14" + fin;
+      img15.style.background = playadelmolino + "15" + fin;
+      img16.style.background = playadelmolino + "16" + fin;
+      img17.style.background = playadelmolino + "17" + fin;
+      img18.style.background = playadelmolino + "18" + fin;
+      img19.style.background = playadelmolino + "19" + fin;
+      img20.style.background = playadelmolino + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "playalascanteras") {
+      img1.style.background = playalascanteras + "1" + fin;
+      img2.style.background = playalascanteras + "2" + fin;
+      img3.style.background = playalascanteras + "3" + fin;
+      img4.style.background = playalascanteras + "4" + fin;
+      img5.style.background = playalascanteras + "5" + fin;
+      img6.style.background = playalascanteras + "6" + fin;
+      img7.style.background = playalascanteras + "7" + fin;
+      img8.style.background = playalascanteras + "8" + fin;
+      img9.style.background = playalascanteras + "9" + fin;
+      img10.style.background = playalascanteras + "10" + fin;
+      img11.style.background = playalascanteras + "11" + fin;
+      img12.style.background = playalascanteras + "12" + fin;
+      img13.style.background = playalascanteras + "13" + fin;
+      img14.style.background = playalascanteras + "14" + fin;
+      img15.style.background = playalascanteras + "15" + fin;
+      img16.style.background = playalascanteras + "16" + fin;
+      img17.style.background = playalascanteras + "17" + fin;
+      img18.style.background = playalascanteras + "18" + fin;
+      img19.style.background = playalascanteras + "19" + fin;
+      img20.style.background = playalascanteras + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "monumentos") {
+      img1.style.background = monumentos + "1" + fin;
+      img2.style.background = monumentos + "2" + fin;
+      img3.style.background = monumentos + "3" + fin;
+      img4.style.background = monumentos + "4" + fin;
+      img5.style.background = monumentos + "5" + fin;
+      img6.style.background = monumentos + "6" + fin;
+      img7.style.background = monumentos + "7" + fin;
+      img8.style.background = monumentos + "8" + fin;
+      img9.style.background = monumentos + "9" + fin;
+      img10.style.background = monumentos + "10" + fin;
+      img11.style.background = monumentos + "11" + fin;
+      img12.style.background = monumentos + "12" + fin;
+      img13.style.background = monumentos + "13" + fin;
+      img14.style.background = monumentos + "14" + fin;
+      img15.style.background = monumentos + "15" + fin;
+      img16.style.background = monumentos + "16" + fin;
+      img17.style.background = monumentos + "17" + fin;
+      img18.style.background = monumentos + "18" + fin;
+      img19.style.background = monumentos + "19" + fin;
+      img20.style.background = monumentos + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "flora") {
+      img1.style.background = flora + "1" + fin;
+      img2.style.background = flora + "2" + fin;
+      img3.style.background = flora + "3" + fin;
+      img4.style.background = flora + "4" + fin;
+      img5.style.background = flora + "5" + fin;
+      img6.style.background = flora + "6" + fin;
+      img7.style.background = flora + "7" + fin;
+      img8.style.background = flora + "8" + fin;
+      img9.style.background = flora + "9" + fin;
+      img10.style.background = flora + "10" + fin;
+      img11.style.background = flora + "11" + fin;
+      img12.style.background = flora + "12" + fin;
+      img13.style.background = flora + "13" + fin;
+      img14.style.background = flora + "14" + fin;
+      img15.style.background = flora + "15" + fin;
+      img16.style.background = flora + "16" + fin;
+      img17.style.background = flora + "17" + fin;
+      img18.style.background = flora + "18" + fin;
+      img19.style.background = flora + "19" + fin;
+      img20.style.background = flora + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "faunageneral") {
+      img1.style.background = faunageneral + "1" + fin;
+      img2.style.background = faunageneral + "2" + fin;
+      img3.style.background = faunageneral + "3" + fin;
+      img4.style.background = faunageneral + "4" + fin;
+      img5.style.background = faunageneral + "5" + fin;
+      img6.style.background = faunageneral + "6" + fin;
+      img7.style.background = faunageneral + "7" + fin;
+      img8.style.background = faunageneral + "8" + fin;
+      img9.style.background = faunageneral + "9" + fin;
+      img10.style.background = faunageneral + "10" + fin;
+      img11.style.background = faunageneral + "11" + fin;
+      img12.style.background = faunageneral + "12" + fin;
+      img13.style.background = faunageneral + "13" + fin;
+      img14.style.background = faunageneral + "14" + fin;
+      img15.style.background = faunageneral + "15" + fin;
+      img16.style.background = faunageneral + "16" + fin;
+      img17.style.background = faunageneral + "17" + fin;
+      img18.style.background = faunageneral + "18" + fin;
+      img19.style.background = faunageneral + "19" + fin;
+      img20.style.background = faunageneral + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "faunacamaleones") {
+      img1.style.background = faunacamaleones + "1" + fin;
+      img2.style.background = faunacamaleones + "2" + fin;
+      img3.style.background = faunacamaleones + "3" + fin;
+      img4.style.background = faunacamaleones + "4" + fin;
+      img5.style.background = faunacamaleones + "5" + fin;
+      img6.style.background = faunacamaleones + "6" + fin;
+      img7.style.background = faunacamaleones + "7" + fin;
+      img8.style.background = faunacamaleones + "8" + fin;
+      img9.style.background = faunacamaleones + "9" + fin;
+      img10.style.background = faunacamaleones + "10" + fin;
+      img11.style.background = faunacamaleones + "11" + fin;
+      img12.style.background = faunacamaleones + "12" + fin;
+      img13.style.background = faunacamaleones + "13" + fin;
+      img14.style.background = faunacamaleones + "14" + fin;
+      img15.style.background = faunacamaleones + "15" + fin;
+      img16.style.background = faunacamaleones + "16" + fin;
+      img17.style.background = faunacamaleones + "17" + fin;
+      img18.style.background = faunacamaleones + "18" + fin;
+      img19.style.background = faunacamaleones + "19" + fin;
+      img20.style.background = faunacamaleones + "20" + fin;
+      claseseleccion()
+
+   }  else if (imagen == "fiestasyeventos") {
+      img1.style.background = fiestasyeventos + "1" + fin;
+      img2.style.background = fiestasyeventos + "2" + fin;
+      img3.style.background = fiestasyeventos + "3" + fin;
+      img4.style.background = fiestasyeventos + "4" + fin;
+      img5.style.background = fiestasyeventos + "5" + fin;
+      img6.style.background = fiestasyeventos + "6" + fin;
+      img7.style.background = fiestasyeventos + "7" + fin;
+      img8.style.background = fiestasyeventos + "8" + fin;
+      img9.style.background = fiestasyeventos + "9" + fin;
+      img10.style.background = fiestasyeventos + "10" + fin;
+      img11.style.background = fiestasyeventos + "11" + fin;
+      img12.style.background = fiestasyeventos + "12" + fin;
+      img13.style.background = fiestasyeventos + "13" + fin;
+      img14.style.background = fiestasyeventos + "14" + fin;
+      img15.style.background = fiestasyeventos + "15" + fin;
+      img16.style.background = fiestasyeventos + "16" + fin;
+      img17.style.background = fiestasyeventos + "17" + fin;
+      img18.style.background = fiestasyeventos + "18" + fin;
+      img19.style.background = fiestasyeventos + "19" + fin;
+      img20.style.background = fiestasyeventos + "20" + fin;
+      claseseleccion()
+
+   } else if (imagen == "puestasdesol") {
+      img1.style.background = puestasdesol + "1" + fin;
+      img2.style.background = puestasdesol + "2" + fin;
+      img3.style.background = puestasdesol + "3" + fin;
+      img4.style.background = puestasdesol + "4" + fin;
+      img5.style.background = puestasdesol + "5" + fin;
+      img6.style.background = puestasdesol + "6" + fin;
+      img7.style.background = puestasdesol + "7" + fin;
+      img8.style.background = puestasdesol + "8" + fin;
+      img9.style.background = puestasdesol + "9" + fin;
+      img10.style.background = puestasdesol + "10" + fin;
+      img11.style.background = puestasdesol + "11" + fin;
+      img12.style.background = puestasdesol + "12" + fin;
+      img13.style.background = puestasdesol + "13" + fin;
+      img14.style.background = puestasdesol + "14" + fin;
+      img15.style.background = puestasdesol + "15" + fin;
+      img16.style.background = puestasdesol + "16" + fin;
+      img17.style.background = puestasdesol + "17" + fin;
+      img18.style.background = puestasdesol + "18" + fin;
+      img19.style.background = puestasdesol + "19" + fin;
+      img20.style.background = puestasdesol + "20" + fin;
+      claseseleccion()
+
+   } else {
+      alert("Hay un error y no se está cargando la galería correctamente")
+   }
+}
