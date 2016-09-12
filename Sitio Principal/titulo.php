e51b09ab6ee1c8b43307d752ab2391ec2c7b2e5b
fryntiz <fryntiz@gmail.com>
2016-09-13 00:49:08 +0200

    estructura básica para títulos

---
+++ Sitio Principal/titulo.php
@@ -0,0 +1,21 @@
+<DIV id="cajaTitulo">
+	<DIV>
+		<H1>
+			Desde Chipiona
+		</H1>
+
+		<P>
+			El proyecto Desde Chipiona trata....
+		</P>
+	</DIV>
+	
+	<DIV>
+		<H2>
+			Que ofrece este lugar
+		</H2>
+
+		<P>
+			En este sitio web encontrarás....
+		</P>
+	</DIV>
+</DIV>
