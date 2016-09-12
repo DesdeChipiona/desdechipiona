5980111bc49e3a5ad2c250a1e07fdadb21661def
fryntiz <fryntiz@gmail.com>
2016-09-13 00:48:37 +0200

    Añadido contenido con ejemplos

---
+++ Sitio Principal/contenido.php
@@ -0,0 +1,43 @@
+<DIV id="cajaContenido">
+	<SECTION>
+		<ARTICLE>
+			Contenido - ARTICLE
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 1
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 2
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 3
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 4
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 5
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 6
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 7
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 8
+		</ARTICLE>
+		
+		<ARTICLE>
+			Contenido - ARTICLE 9
+		</ARTICLE>
+	</SECTION>
+</DIV>
