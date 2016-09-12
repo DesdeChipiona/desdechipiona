6d5c79a49b7185905ce20199fcee7360d3a89340
fryntiz <fryntiz@gmail.com>
2016-09-13 01:02:19 +0200

    Estructura básica para footer

---
+++ Sitio Principal/footer.php
@@ -0,0 +1,7 @@
+<FOOTER>
+	<ADDRESS>
+		Proyecto creado por....
+		<BR/>
+		Correo --> <A href="mailto:info@desdechipiona.es" title="Correo Desde Chipiona">info@desdechipiona.es</A>
+	</ADDRESS>
+</FOOTER>
