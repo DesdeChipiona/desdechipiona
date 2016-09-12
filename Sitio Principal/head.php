447f2d92b05f1323a4aa9f78120a14fdf66591e4
fryntiz <fryntiz@gmail.com>
2016-09-13 00:48:50 +0200

    añadidas hojas de estilos que faltaban

--- Sitio Principal/head.php
+++ Sitio Principal/head.php
@@ -6,5 +6,7 @@
 <LINK rel="stylesheet" href="./CSS/menu.css"/>
 <LINK rel="stylesheet" href="./CSS/head.css"/>
 <LINK rel="stylesheet" href="./CSS/footer.css"/>
+<LINK rel="stylesheet" href="./CSS/titulo.css"/>
+<LINK rel="stylesheet" href="./CSS/contenido.css"/>
 <SCRIPT src="JS/scripts.js"></SCRIPT>
 <SCRIPT src="JS/jquery.js"></SCRIPT>
