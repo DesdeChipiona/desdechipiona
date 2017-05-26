2d0b41e5f04fc73c9341a4aca7716ffaf0a1f58e
fryntiz <fryntiz@gmail.com>
2017-05-27 01:13:51 +0200

    Metaetiquetas mejoradas

--- Sitio Principal/head.php
+++ Sitio Principal/head.php
@@ -1,5 +1,26 @@
 <TITLE>Desde Chipiona, Web oficial</TITLE>
-<META charset="utf-8"/>
+<META charset="utf-8" />
+
+<META name="author" lang="es" content="Raúl Caro Pastorino, tecnico@fryntiz.es" />
+<META name="organization" content="fryntiz" />
+<META name='copyright' content='DesdeChipiona' />
+<META name='language' content='ES' />
+<META name="robots" content="index, follow" />
+<META name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<META property='fb:admins' content='fryntiz' />
+
+<META property="og:locale" content="es_ES" />
+<META property="og:locale:alternate" content="es_LA" />
+<META property='og:url' content='http://www.desdechipiona.es/' />
+<META property='og:site_name' content='Desde Chipiona' />
+<META property='og:type' content='website' />
+
+<META name="twitter:card" content="summary" />
+<META name="twitter:site" content="@fryntiz" />
+<META name="twitter:creator" content="@fryntiz" />
+<META name='twitter:image' content='http://www.desdechipiona.es/images/logo.png' />
+
 <LINK rel="shortcut icon" href="./images/favicon.png"/>
 <LINK rel="stylesheet" href="./CSS/1000.css"/>
 <LINK rel="stylesheet" href="./CSS/style.css"/>
@@ -8,5 +29,10 @@
 <LINK rel="stylesheet" href="./CSS/footer.css"/>
 <LINK rel="stylesheet" href="./CSS/titulo.css"/>
 <LINK rel="stylesheet" href="./CSS/contenido.css"/>
+<!--
+<LINK href=“mini.css” rel=“stylesheet” type=“text/css” media=“screen and (min-width: 981px)” />
+<LINK href=“medium.css” rel=“stylesheet” type=“text/css” media=“screen and (min-width: 481px) and (max-width: 980px)” />
+<LINK href=“mini.css” rel=“stylesheet” type=“text/css” media=“screen and (min-width: 480px)” />
+-->
 <SCRIPT src="JS/scripts.js"></SCRIPT>
 <SCRIPT src="JS/jquery.js"></SCRIPT>
