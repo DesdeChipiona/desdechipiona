9aff8d554bf446c351691f043570d0798ca8fb38
fryntiz <fryntiz@gmail.com>
2017-05-27 17:19:16 +0200

    Descripción en el título

--- Sitio Principal/titulo.php
+++ Sitio Principal/titulo.php
@@ -5,7 +5,9 @@
 		</H1>
 
 		<P>
-			El proyecto Desde Chipiona trata....
+			Este es un proyecto que busca la colaboración para agrupar en un sitio web imágenes, cultura, ideas, otros proyectos, información sobre esta ciudad..
+			<BR />
+			De esta forma podremos aprender más sobre nuestra ciudad y, además, compartir esta información con el resto del mundo pretendiendo hacer que el turismo no sea estacional. Incitar a que nos visiten en otras épocas del año ya que en verano tenemos turistas 
 		</P>
 	</DIV>
 	
