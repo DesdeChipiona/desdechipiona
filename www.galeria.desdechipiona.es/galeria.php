014eb6ad68fa7d8e050e2b9d2b08a47438f453ee
fryntiz <fryntiz@gmail.com>
2017-05-27 22:18:24 +0200

    Importada base de galería

---
+++ www.galeria.desdechipiona.es/galeria.php
@@ -0,0 +1,38 @@
+<DIV id="carruselZ">
+	<DIV id="cajaselectorimagenes">
+
+		<DIV id="cajacarrusel">
+			<DIV id="carruselsuperior"></DIV>
+			<DIV id="cajacontrolimagen">
+				<DIV id="controlimagen">
+					<DIV id="atras" class="todoslosefectos"></DIV>
+					<DIV id="blancoynegro" class="todoslosefectos"></DIV>
+					<DIV id="ampliar" class="todoslosefectos"></DIV>
+					<DIV id="adelante" class="todoslosefectos"></DIV>
+				</DIV>
+			</DIV>
+			<DIV id="cajaflechaizq" class="cajasflechas">
+				<DIV id="flechaizq"></DIV>
+			</DIV>
+
+			<DIV id="cajaflechadrh" class="cajasflechas">
+				<DIV id="flechadrh"></DIV>
+			</DIV>
+
+			<IMG id="fondoimagen" src="images/fondoCarrusel.jpg" alt="carrusel"/>
+
+			<DIV id="centrarimagenactual">
+				<IMG id="imagenactual" src="images/presentacion.png" alt="presentación"/>
+			</DIV>
+
+			<DIV id="carruselinferior"></DIV>
+		</DIV>
+
+	</DIV>
+
+	<?php
+		include 'carrusel.php';
+	?>
+</DIV>
+
+
