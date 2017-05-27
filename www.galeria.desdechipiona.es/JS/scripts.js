d1f3ab43f77809d9bdac9add34e377d5dfbebc25
fryntiz <fryntiz@gmail.com>
2017-05-28 01:31:11 +0200

    Función para ver versión actual

---
+++ www.galeria.desdechipiona.es/JS/scripts.js
@@ -0,0 +1,3 @@
+function versionActual() {
+	alert("La versión actual de este sitio web está en pruebas\n Actualmente la version es la v0.4\n Existen aún muchos errores que estamos corrigiendo");
+}
