5b1167b118eedbab779c4eb6d32d6cc0f7290bb6
fryntiz <fryntiz@gmail.com>
2017-05-27 19:38:40 +0200

    Modificada estructura para el título

--- Sitio Principal/titulo.php
+++ Sitio Principal/titulo.php
@@ -1,23 +1,13 @@
 <DIV id="cajaTitulo">
-	<DIV>
+	<DIV id="subcajaTitulo">
 		<H1>
 			Desde Chipiona
 		</H1>
 
-		<P>
+		<P class="subtitulo">
 			Este es un proyecto que busca la colaboración para agrupar en un sitio web imágenes, cultura, ideas, otros proyectos, información sobre esta ciudad..
 			<BR />
 			De esta forma podremos aprender más sobre nuestra ciudad y, además, compartir esta información con el resto del mundo pretendiendo hacer que el turismo no sea estacional. Incitar a que nos visiten en otras épocas del año ya que en verano tenemos turistas 
-		</P>
-	</DIV>
-	
-	<DIV>
-		<H2>
-			Que ofrece este lugar
-		</H2>
-
-		<P>
-			En este sitio web encontrarás....
 		</P>
 	</DIV>
 </DIV>
