5cefdabcd16aac8198873e55b70f754104e2222b
fryntiz <fryntiz@gmail.com>
2017-05-27 22:18:49 +0200

    Reparado título

--- www.galeria.desdechipiona.es/titulo.php
+++ www.galeria.desdechipiona.es/titulo.php
@@ -1,13 +1,7 @@
 <DIV id="cajaTitulo">
 	<DIV id="subcajaTitulo">
 		<H1>
-			Desde Chipiona
+			Desde Chipiona, Galería
 		</H1>
-
-		<P class="subtitulo">
-			Este es un proyecto que busca la colaboración para agrupar en un sitio web imágenes, cultura, ideas, otros proyectos, información sobre esta ciudad..
-			<BR />
-			De esta forma podremos aprender más sobre nuestra ciudad y, además, compartir esta información con el resto del mundo pretendiendo hacer que el turismo no sea estacional. Incitar a que nos visiten en otras épocas del año ya que en verano tenemos turistas
-		</P>
 	</DIV>
 </DIV>
