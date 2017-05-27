097df7869f05675ca813032217b56725c7f88fc7
fryntiz <fryntiz@gmail.com>
2017-05-28 01:04:56 +0200

    Agregado script

---
+++ www.galeria.desdechipiona.es/galeria_6_puestasdesol.php
@@ -0,0 +1,62 @@
+<!--Selector de imágenes-->
+	<UL id="ulselectorimagen"> <!--Con PHP traigo las imágenes por lotes del directorio y proceso con JS-->
+		<?php
+		header('Content-Type: text/html; charset=UTF-8');
+		$ArrayImagenes = array();
+		$ArrayImagenesLongitud = "";
+		$DirectorioImagenes = "images/7_puestas_de_sol/thumbnail/";
+		$DirectorioImagenesHigh = "images/7_puestas_de_sol/high/";
+
+			function playas()
+			{
+				global $ArrayImagenes;
+				global $ArrayImagenesLongitud;
+				global $DirectorioImagenes;
+				global $DirectorioImagenesHigh;
+				$DirectorioActual = dir($DirectorioImagenes);//Leer directorio y devolver array
+
+				while (($archivo = $DirectorioActual->read()) !== false)//Bucle que crea array
+					{
+						if (eregi("gif", $archivo) || eregi("jpg", $archivo) || eregi("png", $archivo)){//Filtro solo imágenes
+							array_push($ArrayImagenes, $archivo);
+						}
+					}
+				$DirectorioActual->close();
+				sort($ArrayImagenes);//Ordenar array
+				$ArrayImagenesLongitud = count($ArrayImagenes);
+
+				$x = 0;
+				while ($x < $ArrayImagenesLongitud) //Bucle que pinta el array
+					{
+						echo '<LI><IMG class="selectorimagen" onclick="cambiarBackgroundHigh('."$ArrayImagenesLongitud, '$DirectorioImagenesHigh', '$x'".')" src="'.$DirectorioImagenes.$ArrayImagenes[$x].'"></LI>'."\n";
+						$x++;
+					}
+				return $ArrayImagenes;//Devolver valor
+			}
+
+		playas();
+	?>
+	</UL>
+</DIV><!--Este div cierra el bloque, leer más abajo el comentario-->
+
+<DIV id="cajacarrusel">
+	<DIV id="cajacontrolimagen">
+		<DIV id="controlimagen">
+			<DIV id="atras" class="todoslosefectos"></DIV>
+			<DIV id="blancoynegro" class="todoslosefectos"></DIV>
+			<DIV id="ampliar" class="todoslosefectos"></DIV>
+			<DIV id="adelante" class="todoslosefectos"></DIV>
+		</DIV>
+	</DIV>
+	<DIV id="cajaflechaizq" class="cajasflechas">
+		<DIV id="flechaizq"></DIV>
+	</DIV>
+
+	<DIV id="cajaflechadrh" class="cajasflechas">
+		<DIV id="flechadrh"></DIV>
+	</DIV>
+
+	<DIV id="centrarimagenactual">
+		<IMG id="imagenactual" src="images/presentacion.png" alt="presentación"/>
+	</DIV>
+<!--El DIV que falta corresponde al de cierre en "galeria.php" para evitar error al importar con "include" ya que se encuentra el bloque para el id="cajaselectorimagenes" inclompleto si aquí lo colocase.
