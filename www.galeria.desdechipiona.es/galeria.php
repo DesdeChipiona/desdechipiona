632acd56b5d73dd40ae0d0cad919b7a94fcc45c1
fryntiz <fryntiz@gmail.com>
2017-05-27 23:40:36 +0200

    Añadido carrusel

--- www.galeria.desdechipiona.es/galeria.php
+++ www.galeria.desdechipiona.es/galeria.php
@@ -50,7 +50,30 @@
 		</UL>
 	</DIV>
 
-	<?php
-		include 'carrusel.php';
-	?>
+	<DIV id="cajacarrusel">
+		<DIV id="carruselsuperior"></DIV>
+		<DIV id="cajacontrolimagen">
+			<DIV id="controlimagen">
+				<DIV id="atras" class="todoslosefectos"></DIV>
+				<DIV id="blancoynegro" class="todoslosefectos"></DIV>
+				<DIV id="ampliar" class="todoslosefectos"></DIV>
+				<DIV id="adelante" class="todoslosefectos"></DIV>
+			</DIV>
+		</DIV>
+		<DIV id="cajaflechaizq" class="cajasflechas">
+			<DIV id="flechaizq"></DIV>
+		</DIV>
+
+		<DIV id="cajaflechadrh" class="cajasflechas">
+			<DIV id="flechadrh"></DIV>
+		</DIV>
+
+		<IMG id="fondoimagen" src="images/fondoCarrusel.jpg" alt="carrusel"/>
+
+		<DIV id="centrarimagenactual">
+			<IMG id="imagenactual" src="images/presentacion.png" alt="presentación"/>
+		</DIV>
+
+		<DIV id="carruselinferior"></DIV>
+	</DIV>
 </DIV>
